a87ac4d9bf7d327c534c4f0e375f55b8585642d6
HeilanElf <3263718994@qq.com>
2023-06-15 14:29:05 +0000

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,17 @@
 import 'package:flutter/material.dart';
-import 'todolist.dart';
-import 'todolist.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/services.dart';
+import 'ToList.dart';
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int _selectedTabIndex = 0;
-
-  List<Widget> _tabList = [
-   AddTodoScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedTabIndex = index;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tab Bar App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Tab Bar App'),
-        ),
-        backgroundColor: Color.fromARGB(255, 168, 206, 244), //这里设置为灰色
-        body: _tabList[_selectedTabIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label: 'Add',
-            ),
-            
-          ],
-          currentIndex: _selectedTabIndex,
-          onTap: _onItemTapped,
-        ),
-      ),
+      title: 'Todo List',
+      home: TodoListPage(),
     );
   }
 }
